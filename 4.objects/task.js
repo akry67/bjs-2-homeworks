3170f2fbf775603db58ac27e96c7642fd766b484
Anna Kuznetsova <akry67@gmail.com>
2022-09-18 22:23:41 +0400

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    // добавить первую оценку 
+    this.marks = [mark];
+  } else {
+    // добавить вторую и последующие оценки в массив
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function marks(...args) { // rest параметр, т.к. неизвестно кол-во параметров
+  if (this.marks === undefined) {
+    this.marks = [...args];
+  } else {
+    this.marks.push(...args);
+  }
+}
+
+Student.prototype.getAverage = function (average) {
+  average = (this.marks.reduce((accumulator, currentNumber) => accumulator + currentNumber, 0) / this.marks.length);
+  return average;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
